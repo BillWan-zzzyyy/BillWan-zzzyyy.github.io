b3921600d67af6b7b8f22f6e096aa3e57b9b77ba
Zhengyang.Wan <75086412+BillWan-zzzyyy@users.noreply.github.com>
2025-10-03 19:01:37 -0500

    Update google-scholar-citations.rb

--- _plugins/google-scholar-citations.rb
+++ _plugins/google-scholar-citations.rb
@@ -32,7 +32,15 @@ module Jekyll
           sleep(rand(5.5..7.5))
 
           # Fetch the article page
-          doc = Nokogiri::HTML(URI.open(article_url, "User-Agent" => "Ruby/#{RUBY_VERSION}"))
+        
+          user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
+
+          headers = {
+            "User-Agent" => user_agent,
+            "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "Accept-Language" => "en-US,en;q=0.9"
+          }
+          doc = Nokogiri::HTML(URI.open(article_url, headers))
 
           # Attempt to extract the "Cited by n" string from the meta tags
           citation_count = 0
